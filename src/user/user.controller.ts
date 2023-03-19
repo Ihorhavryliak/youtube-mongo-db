9e93919884ff799f4e7fade607c6a5610566053e
Ihor <kachkamala@gmail.com>
2023-03-19 23:55:41 +0200

    validators

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,20 +1,34 @@
-
 import { UserService } from './user.service';
-import { Body, Controller, Get, Post } from "@nestjs/common";
+import {
+  BadRequestException,
+  Body,
+  Controller,
+  Get,
+  Param,
+  ParseArrayPipe,
+  ParseBoolPipe,
+  ParseIntPipe,
+  Post,
+  Query,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
 import { CreateUserDto } from './userDto/create-user.dto';
-
-
 
 @Controller()
 export class UserController {
-  constructor(private userService: UserService){}
+  constructor(private userService: UserService) {}
   @Post()
-  async createUser(@Body() createUserDto: CreateUserDto){
-    return this.userService.createUser(createUserDto)
+
+  async createUser(@Body(new ParseArrayPipe({items: CreateUserDto})) createUserDto: CreateUserDto[]) {
+    return this.userService.createUser(createUserDto[0]);
   }
 
   @Get()
-  async findAll(): Promise<any>{
+  async findAll(
+    @Query('ids', new ParseArrayPipe({items: Number, separator: ','}))
+    ids: number[],
+  ) {
     return this.userService.findAll();
   }
 }
--- src/user/userDto/create-user.dto.ts
+++ src/user/userDto/create-user.dto.ts
@@ -1,6 +1,16 @@
-
+import {IsEmail, IsNotEmpty, IsNumberString} from 'class-validator'
+import {IntersectionType, OmitType, PartialType, PickType} from '@nestjs/mapped-types'
 
 export class CreateUserDto{
   readonly name: string
   readonly password: string
+  readonly lastName: string
+
 }
+export class CreateColorsDto{
+  readonly color: string
+}
+
+export class UpdateUserDto extends PartialType(
+  OmitType(CreateUserDto, ['name'] as const)
+) {}
